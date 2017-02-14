2a4db388107706b847b984b491dbbaafe7bb1810
Ricky Grams <rickygrams@Rickys-MacBook-Pro.local>
2017-02-13 16:46:01 -0800

    everything but new letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,18 +6,27 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var guessesSoFar = [];
+var newLetter = [];
+
 
 // computer chooses random letter from array
-var randomLetter = letters[Math.floor(Math.random() * letters.length)];
+// var randomLetter = letters[Math.floor(Math.random() * letters.length)];
 
 // *********FUNCTIONS*********************
 
 function reset() {
 	guessesLeft = 9;
 	displayGuessesLeft()
-	guessesSoFar = [];
-	// NEED NEW RANDOM LETTER???????????????????????
+	guessesSoFar.length = 0;
+	displayGuessesSoFar();
+	//????????????? NEED NEW RANDOM LETTER???????????????????????
+	newLetter.length = 0;
+	generateLetter();
+}
+
+function generateLetter() {
 	var randomLetter = letters[Math.floor(Math.random() * letters.length)];
+	newLetter.push(randomLetter);
 }
 
 function displayGuessesLeft() {
@@ -25,41 +34,50 @@ function displayGuessesLeft() {
 }
 
 function displayGuessesSoFar() {
-	document.querySelector("#yourGuessesSoFar").innerHTML = "Your Guesses so far: " + guessesSoFar.push(', ');
+	document.querySelector("#yourGuessesSoFar").innerHTML = "Your Guesses so far: " + guessesSoFar.join(" ");
 }
 
 function wrongGuess() {
 	guessesLeft--;
 	displayGuessesLeft();
-	// Need to fill empty array with wrong guesses???????????????????????
-	// guessesSoFar.push(userGuess);
+	if (guessesLeft === 1) {
+		alert("You have one more guess left!");
+	}
 }
+// ************************************************
 
+
+
+
+generateLetter();
+console.log(newLetter);
 displayGuessesLeft();
 
 // ***************MAIN PROCESS*******************
 // user presses key
 document.onkeyup = function(event) {	
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-			console.log(userGuess);
 		
-		if ((guessesLeft > 0) && (userGuess === randomLetter)) {
+		guessesSoFar.push(userGuess);
+		displayGuessesSoFar();
+		
+		if ((guessesLeft > 0) && (userGuess === newLetter)) {
 			wins++;
 			document.querySelector("#wins").innerHTML = "Wins: " + wins;
 			alert("You are a Pyschic!");
 			reset();
 		}
-		if ((guessesLeft > 0) && (userGuess !== randomLetter)) {
+		if ((guessesLeft > 0) && (userGuess !== newLetter)) {
 			wrongGuess();
 		}
 		if (guessesLeft === 0) {
 			losses++;
 			document.querySelector("#losses").innerHTML = "Losses: " + losses;
 			alert("Psychics aren't real!");
-			displayGuessesLeft();
 			reset();
 		}
 }
+// humanbraincode
 // computer chooses random letter
 // user guesses a letter
 // 	if userGuess = random letter
